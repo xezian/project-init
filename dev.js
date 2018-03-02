cb13e6490b9c38898705aeabe2fb02be709491e2
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-01 19:26:45 -0700

    update dev script for windows

--- dev.js
+++ dev.js
@@ -1,5 +1,10 @@
 var exec = require( "child_process" ).exec;
 
+var parent = exec( "node server.js ");
+
+parent.stdout.pipe( process.stdout );
+parent.stderr.pipe( process.stderr );
+
 var child = exec( "npm start", {
 	cwd: process.cwd() + "/client"
 });
