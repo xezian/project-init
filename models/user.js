a16b08489f8256d4de02ba8d23fefeac15fdb0e7
Christopher Rolls <imperialrolls@gmail.com>
2018-03-07 20:15:19 -0700

    I am in the future present

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
+    salt: DataTypes.STRING,
   }, {timestamps: false});
   User.associate = function(models) {
     // When a User is deleted, also delete any associated comments
